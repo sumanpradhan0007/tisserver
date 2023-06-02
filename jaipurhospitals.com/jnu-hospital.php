8ec3a988076b00dd341b57593efa7bc6355690dd
Suman Pradhan <suman007.pradhan@gmail.com>
2023-06-02 21:09:43 +0530

    2nd changes
    this second

--- jaipurhospitals.com/jnu-hospital.php
+++ jaipurhospitals.com/jnu-hospital.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JNU Hospital</title>
+    <title>JNU Hospital 2nd Changes by git</title>
 
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/all.min.css" rel="stylesheet">
